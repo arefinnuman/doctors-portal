132b601584f57c2b0b680bbca9d076db0823229d
arefin <arefinnuman2000@gmail.com>
2022-11-12 12:10:27 +0600

    Footer Added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { router } from "./Routes/Routes";
 
 function App() {
   return (
-    <div className="max-w-[1440px] mx-auto">
+    <div>
       <RouterProvider router={router} />
     </div>
   );
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import footer from "../../../Assets/images/footer.png";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <section style={{ background: `url(${footer})`, backgroundSize: "cover" }}>
+      <footer className="footer mt-10 p-10   text-neutral">
+        <div>
+          <span className="footer-title">Services</span>
+          <Link className="link link-hover">Monthly Checkup</Link>
+          <Link className="link link-hover">WeekLy Checkup</Link>
+          <Link className="link link-hover">Deep Checkup</Link>
+          <Link className="link link-hover">Emergency Checkup</Link>
+        </div>
+        <div>
+          <span className="footer-title">Oral Health</span>
+          <Link className="link link-hover">Fluoride Treatment</Link>
+          <Link className="link link-hover">Cavity Filing</Link>
+          <Link className="link link-hover">Teath Whitening</Link>
+        </div>
+        <div>
+          <span className="footer-title">Our Address</span>
+          <p className="text-secondary font-extrabold">
+            Mirpur-1, Dhaka, Bangladesh
+          </p>
+        </div>
+      </footer>
+      <footer className="footer footer-center mb-5 text-base-content">
+        <div>
+          <p>Copyright © 2022 - All right reserved by 24hr HomeCare</p>
+        </div>
+      </footer>
+    </section>
   );
 };
 
